f80f755048381fcc24fc223ad5206ca2449959dc
Aayush Gulia <aayushgulia07@outlook.com>
2021-09-24 23:31:03 +0530

    first swing commit

---
+++ src/com/company/MyFrame.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+    MyFrame(){
+        JFrame frame = new JFrame();
+        frame.setVisible(true); // set visibility
+        frame.setResizable(true); // is frame resizeable
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //default close operation; EXIT ON CLOSE
+        frame.setSize(840, 840); // set x-dim & set y-dim
+        frame.setTitle("ATOM"); // set title of the JFrame.
+
+        ImageIcon logoImage = new ImageIcon("images/index.png");
+        frame.setIconImage(logoImage.getImage()); //change icon image.
+
+        frame.getContentPane().setBackground(new Color(128,128,128)); //change color of background.
+    }
+}
